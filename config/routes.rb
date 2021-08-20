39e279df10b2a4a04c5987a9f6a57dceb623a1f8
keigo1401 <totutotu1015@gmail.com>
2021-08-20 14:04:12 +0000

    [Update]check No.17

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :books
   devise_for :users
   root to: 'homes#top'
-
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
