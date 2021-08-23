4887409b9050ff60a333c84c8aba9549abc30940
keigo1401 <totutotu1015@gmail.com>
2021-08-23 14:59:32 +0000

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :post_images, dependent: :destroy
+  
+  validates :name, presence: true, length: {in: 2..20}
+  validates :introduction, length: { maximum: 50}
+  
+  attachment :profile_image
+  
+  def email_required?
+    false
+  end
+  
+  def email_changed?
+    false
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_122444) do
+ActiveRecord::Schema.define(version: 2021_08_23_143057) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.string "image_id"
     t.text "opinion"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "profile_image_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
